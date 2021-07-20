a9f66a1848029a1829efe6036dd8db7ed362dabd
HarryM265 <harrison_miles@outlook.com>
2021-07-20 10:57:00 +0800

    Cleanup

--- lib/screens/Startup/widgets/forms/SignInUserNameFormWidget.dart
+++ lib/screens/Startup/widgets/forms/SignInUserNameFormWidget.dart
@@ -8,19 +8,11 @@ class UsernameCustomForm extends StatefulWidget {
   }
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
 class UsernameCustomFormState extends State<UsernameCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<UsernameCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: Column(
@@ -28,7 +20,6 @@ class UsernameCustomFormState extends State<UsernameCustomForm> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           TextFormField(
-            // The validator receives the text that the user has entered.
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Please enter some text';
@@ -40,12 +31,12 @@ class UsernameCustomFormState extends State<UsernameCustomForm> {
             padding: const EdgeInsets.symmetric(vertical: 10.0),
             child: ElevatedButton(
               onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
                 if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
                   ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Submition Complete!')));
+                    SnackBar(
+                      content: Text('Submition Complete!'),
+                    ),
+                  );
                 }
               },
               child: Text('Submit'),
